1ad523b611edab188f9d8f257ea845748d77acdd
Pineapple <pineapple_cpp@163.com>
2021-09-16 20:43:29 +0800

    集成电子邮件发送功能

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
 
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail, Message
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -16,11 +17,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data.sqlite')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = ('Flasky Admin', app.config['MAIL_USERNAME'])
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -35,6 +45,8 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
 
@@ -94,6 +106,23 @@ def make_shell_context() -> dict:
     注册一个 shell 上下文管理器，启动 flask shell 时可以自动导入数据库实例和模型
     """
     return dict(db=db, User=User, Role=Role)
+
+
+def send_email(to, subject, template, **kwargs):
+    """
+    发送电子邮件
+
+    :param to: 收件人
+    :param subject: 主题
+    :param template: Jinja2模板
+    :param kwargs: 关键字参数
+    :return:
+    """
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 if __name__ == '__main__':
