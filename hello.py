05846919ccf2b67408afa0be5069c2cb4aad8950
Pineapple <pineapple_cpp@163.com>
2021-09-16 12:18:12 +0800

    闪现消息

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -18,6 +18,10 @@ def index():
     form = NameForm()
 
     if form.validate_on_submit():
+        old_name = session.get('name')
+        # 判断是否改变名称，改变则显示提示信息
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
 
