90ebda60830cdae3a516ef675119eeda2f771509
Pineapple <pineapple_cpp@163.com>
2021-09-17 19:52:09 +0800

    注册新用户

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,8 +3,10 @@
 # forms.py - 2021年 九月 17日
 # 登录表单
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, BooleanField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, ValidationError
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -12,3 +14,24 @@ class LoginForm(FlaskForm):
     password = StringField('Password', validators=[(DataRequired())])
     remember_me = BooleanField('Keep me login in')  # 复选框
     submit = SubmitField('Log in')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('Username',
+                           validators=[DataRequired(),
+                                       Length(1, 64),
+                                       Regexp('^[A-Za-z][A-Za-z0-9.]*$', 0,
+                                              'Username must have only letters, numbers, dots or underscores')])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), EqualTo('password2', message='Passwords must mach.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field) -> None:
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field) -> None:
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -6,7 +6,8 @@ from flask import render_template, request, url_for, redirect, flash
 from flask_login import login_user, login_required, logout_user
 
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
+from .. import db
 from ..models import User
 
 
@@ -42,3 +43,17 @@ def logout():
     flash('You have been logged out.')
 
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', form=form)
